8593449b349dfb0f7a055c90f356a6d98c83ccc0
bunni <cserrano@gmail.com>
2022-12-31 19:10:41 -0800

    Detect visual selection coordinates.
    README.md
      - add dependencies
      - update instructions
    
    gpt.py
      - detect start and end cursor position
    
    TODO
      - use cursor position to parse buffer

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # llm.nvim
 
+## Dependencies
+Install [pynvim](https://github.com/neovim/pynvim): `python -m pip install pynvim`
+### Verify pynvim is detected
+`:checkhealth` and verify Python 3 provider shows Python and pynvim
+
 ## Install Plugin
 1. Add to your `init.lua`: `use 'bybunni/llm.nvim' -- llm.nvim local development directory`
 2. Run `:PackerSync`
@@ -7,4 +12,4 @@
 4. Quit and restart Neovim
 
 ## Call Function
-`:call GPTHello()`
+`:call Selection()`
--- rplugin/python3/gpt.py
+++ rplugin/python3/gpt.py
@@ -6,6 +6,15 @@ class GPT:
     def __init__(self, nvim):
         self.nvim = nvim
 
-    @pynvim.function("GPTHello")
-    def hello(self, args):
-        self.nvim.out_write("Hello from Python!\n")
+    @pynvim.function("Selection")
+    def print_selection(self, args):
+        self.nvim.out_write(str(self.selection(args)) + "\n")
+
+    def selection(self, args):
+        # 2147483647 is the maximum value for a signed 32-bit integer and will be returned as cecol if we are at the end of the line
+        _, csrow, cscol, _ = self.nvim.eval('getpos("\'<")')
+        _, cerow, cecol, _ = self.nvim.eval('getpos("\'>")')
+        if csrow < cerow or (csrow == cerow and cscol <= cecol):
+            return csrow - 1, cscol - 1, cerow - 1, cecol
+        else:
+            return cerow - 1, cecol - 1, csrow - 1, cscol
