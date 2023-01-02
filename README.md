619c17ab1b7e006b667a53fab30429787ee16fce
bunni <cserrano@gmail.com>
2023-01-02 15:37:07 -0800

    GPT3 integration with response in window.

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 
 ## Dependencies
 Install [pynvim](https://github.com/neovim/pynvim): `python -m pip install pynvim`
+Install [openai](https://github.com/openai/openai-python): `python -m pip install pynvim`
 ### Verify pynvim is detected
 `:checkhealth` and verify Python 3 provider shows Python and pynvim
-
+## Export OpenAI Key
+`export OPENAI_API_KEY=YourKey`
 ## Install Plugin
 1. Add to your `init.lua`: `use 'bybunni/llm.nvim' -- llm.nvim local development directory`
 2. Run `:PackerSync`
@@ -12,4 +14,8 @@ Install [pynvim](https://github.com/neovim/pynvim): `python -m pip install pynvi
 4. Quit and restart Neovim
 
 ## Call Function
-`:call Selection()`
+Add a binding for `SelectionWindow()` e.g., `vim.keymap.set({ 'n', 'v' }, '<leader>w', ':call SelectionWindow()<CR>') -- llm.nvim`
+
+## Use
+Highlight the desired text to send to GPT-3 as a prompt and call `SelectionWindow()`.
+After a few seconds a window will open on the right with the response.
--- rplugin/python3/gpt.py
+++ rplugin/python3/gpt.py
@@ -1,4 +1,8 @@
 import pynvim
+import os
+import openai
+
+openai.api_key = os.environ["OPENAI_API_KEY"]
 
 
 @pynvim.plugin
@@ -6,12 +10,39 @@ class GPT:
     def __init__(self, nvim):
         self.nvim = nvim
 
+    @pynvim.function("SelectionWindow")
+    def selection_window(self, args):
+        csrow, cscol, cerow, cecol = self.selection(args)
+        scratch_buffer = self.nvim.api.create_buf(True, False)
+        highlighted_text = self.nvim.current.buffer[csrow : cerow + 1]
+        # prompt = "Write a pragraph based on the below outline:\n\n"
+        prompt = "\n".join(highlighted_text)
+        # prompt += text
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt,
+            max_tokens=100,
+            echo=True,
+        )
+        for line in response.choices[0].text.splitlines():
+            scratch_buffer.append(line)
+        # prompt = "\n\nPrompt sent to GPT3:\n" + prompt
+        # for line in prompt.splitlines():
+        #     scratch_buffer.append(line)
+        window = self.nvim.api.open_win(
+            scratch_buffer,
+            False,
+            {"relative": "win", "row": 0, "col": 95, "width": 88, "height": 20},
+        )
+
     @pynvim.function("Selection")
     def print_selection(self, args):
         self.nvim.out_write(str(self.selection(args)) + "\n")
 
     def selection(self, args):
-        # 2147483647 is the maximum value for a signed 32-bit integer and will be returned as cecol if we are at the end of the line
+        # 2147483647 is the maximum value for a signed 32-bit integer and will
+        # be returned as cecol if we are at the end of the line
+        # https://github.com/vim/vim/issues/4464
         _, csrow, cscol, _ = self.nvim.eval('getpos("\'<")')
         _, cerow, cecol, _ = self.nvim.eval('getpos("\'>")')
         if csrow < cerow or (csrow == cerow and cscol <= cecol):
