2874c4e248099cc9c2905b4bcbfa8d896851f4a4
bunni <cserrano@gmail.com>
2023-02-11 20:25:18 -0800

    Adding CodexCompile command.
    gpt.py
        - CodexCompile
            1. compiles c code
            2. takes the output of the compiler (warnings + errors)
            3. sends 1 and 2 to code-davinci-002 and prompts for correct code
    
    TODO
        - run the returned code and iterate on errors until correct
        - diff the correct output and present the diff as a suggestion

--- rplugin/python3/gpt.py
+++ rplugin/python3/gpt.py
@@ -11,9 +11,35 @@ class GPT:
         self.nvim = nvim
         self.response_buffer = None
 
+    @pynvim.command("CodexCompile", range=True)
+    def codex_compile(self, *args):
+        original_code = self.nvim.current.buffer[:]
+        original_code = "\n".join(original_code)
+        original_window = self.nvim.current.window
+        self.nvim.out_write(original_code)
+        self.nvim.command(":new | read !cc #")
+        compiler_output = self.nvim.current.buffer[:]
+        compiler_output = "\n".join(compiler_output)
+        self.nvim.out_write(compiler_output)
+        prompt = (
+            original_code
+            + "\n\n"
+            + compiler_output
+            + "\n\n"
+            + "Suggest a fix to the code."
+        )
+        self.nvim.out_write("\nWaiting for response...\n")
+        response = openai.Completion.create(
+            model="code-davinci-002",
+            prompt=prompt,
+            max_tokens=256,
+            echo=True,
+        )
+        self.nvim.out_write(response.choices[0].text)
+
     @pynvim.command("Prompt", range=True, nargs="*")
     def prompt(self, *args):
-        self.nvim.out_write(str(len(args)) + str(args) + "\n")
+        # self.nvim.out_write(str(len(args)) + str(args) + "\n")
         csrow, cerow = args[1]
         selected_text = self.nvim.current.buffer[
             csrow - 1 : (cerow + 1) if cerow != csrow else cerow
@@ -29,7 +55,8 @@ class GPT:
         else:
             prompt = selected_text
         # self.nvim.out_write(str([csrow, cscol, cerow, cecol]) + "\n")
-        self.nvim.out_write(str([csrow, cerow]) + "\n")
+        # self.nvim.out_write(str([csrow, cerow]) + "\n")
+        # self.nvim.out_write("Goodbye from Python!\n")
 
         if self.response_buffer is None:
             # create split window for response
@@ -39,12 +66,14 @@ class GPT:
             self.nvim.api.win_set_buf(current_window, self.response_buffer)
 
         # self.response_buffer[:] = ["Waiting for response..."]
+        self.nvim.out_write("Waiting for response...\n")
         response = openai.Completion.create(
             model="text-davinci-003",
             prompt=prompt,
             max_tokens=300,
             echo=True,
         )
+        self.nvim.out_write("\n")
 
         self.response_buffer.append(
             response.choices[0].text.splitlines() + [""], index=0
@@ -53,7 +82,7 @@ class GPT:
 
     @pynvim.command("PromptChat", range=True)
     def prompt_chat(self, *args):
-        self.nvim.out_write(str(len(args)) + str(args) + "\n")
+        # self.nvim.out_write(str(len(args)) + str(args) + "\n")
         buffer_text = self.nvim.current.buffer[:]
         prompt = "\n".join(buffer_text)
 
@@ -76,8 +105,8 @@ class GPT:
         # https://github.com/vim/vim/issues/4464
         a, csrow, cscol, b = self.nvim.eval('getpos("\'<")')
         c, cerow, cecol, d = self.nvim.eval('getpos("\'>")')
-        self.nvim.out_write("ab" + str([a, csrow, cscol, b]) + "\n")
-        self.nvim.out_write("cd" + str([c, cerow, cecol, d]) + "\n")
+        # self.nvim.out_write("ab" + str([a, csrow, cscol, b]) + "\n")
+        # self.nvim.out_write("cd" + str([c, cerow, cecol, d]) + "\n")
         if csrow < cerow or (csrow == cerow and cscol <= cecol):
             return csrow - 1, cscol - 1, cerow - 1, cecol
         else:
